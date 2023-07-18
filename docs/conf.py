c08e107b5a12b8259110a7a4187eeb8b102f0b83
diliberg <53035050+diliberg@users.noreply.github.com>
2023-07-18 11:02:57 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "CNSP Resources"
+copyright = "2023, Giovanni Di Liberto and the CNSP organisers"
+author = "Giovanni Di Liberto"
 
 
 # -- General configuration ---------------------------------------------------
