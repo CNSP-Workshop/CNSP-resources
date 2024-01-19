e2b6b0348c2f504db6134dfd7b4695a851c239c1
diliberg <diliberg@tcd.ie>
2024-01-19 16:31:01 +0000

    a

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
-    "sphinx_rtd_theme",
+    "sphinx-rtd-theme",
 ]
 
 intersphinx_mapping = {
