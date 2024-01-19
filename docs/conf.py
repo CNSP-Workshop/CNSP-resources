4a05758e0cb78908823816aea5687a38506ea19f
diliberg <diliberg@tcd.ie>
2024-01-19 16:42:41 +0000

    a

--- docs/conf.py
+++ docs/conf.py
@@ -56,8 +56,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-python -m pip install sphinx_rtd_theme
-import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
